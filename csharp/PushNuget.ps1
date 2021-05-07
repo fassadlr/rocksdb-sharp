175f02a774f1cd3fcefabcb9c8a16c63d9d3d051
Francois de la Rouviere <fassadlr@gmail.com>
2021-05-07 12:13:49 +0100

    Update PushNuget.ps1

--- csharp/PushNuget.ps1
+++ csharp/PushNuget.ps1
@@ -1,4 +1,4 @@
 rm ".\bin\Release\" -Recurse -Force
 dotnet build --configuration Release
 dotnet pack --configuration Release --include-source --include-symbols 
-dotnet nuget push ".\bin\Release\*.symbols.nupkg" -k "oy2h6m5c22shkewqqadqlzuhhouc5wgezn6zhi3uniu4zy" --source "https://api.nuget.org/v3/index.json"
+dotnet nuget push ".\bin\Release\*.symbols.nupkg" -k "" --source "https://api.nuget.org/v3/index.json"
